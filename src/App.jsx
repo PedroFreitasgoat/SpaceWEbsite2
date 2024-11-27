cd7252fa9c1447497e2988b1c41f1fbad7aae495
PdroFreitass <pedrothdvzs@gmail.com>
2024-11-27 20:15:59 -0300

    Criação de mais um component e arrumando um Bug

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import bgVideo from './assets/earth-bg.mp4'
+import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
+import Services from './components/Services/Services'
+import Banner from './components/Banner/Banner'
+import Banner2 from './components/Banner/Banner2'
+
+const App = () => {
+  return (
+    <div className=''>
+      <div className='h-[700px] relative'> 
+          <video 
+          autoPlay 
+          loop muted 
+          className='fixed right-0 top-0 h-[700px] w-full object-cover z-[-1]'>
+            <source src={bgVideo} type='video/mp4'/>
+          </video>
+          <Navbar />
+          <Hero />
+      </div>
+
+      {/* card section */}
+      <Services />
+      <Banner />
+      <Banner2 />
+    </div>
+  )
+}
+
+export default App
